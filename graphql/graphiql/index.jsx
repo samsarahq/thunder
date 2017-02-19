567339ac684c7cf9f676ad89ff29ba4ee4b52c62
Jelle van den Hooff <jelle@vandenhooff.name>
2017-02-18 21:46:09 -0800

    graphiql: add a standalone graphiql package
    To make it easy to get started using Thunder, introduce a new graphiql
    package that embeds graphiql. You can use the package without needing
    any local JavaScript tooling installed, as the compiled JavaScript is
    embedded using rakyll/statik into the Go source.
    
    Intended usage:
    
        schema := builder.MustBuild()
        introspection.AddIntrospectionToSchema(schema)
        http.Handle("/graphql", graphql.Handler(schema))
        http.Handle("/graphiql/", http.StripPrefix("/graphiql/", graphiql.Handler()))
        if err := http.ListenAndServe(":3000", nil); err != nil {
            log.Fatal(err)
        }

---
+++ graphql/graphiql/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GraphiQL from 'graphiql';
+import { connection } from 'thunder-react';
+
+import './node_modules/graphiql/graphiql.css';
+
+function graphQLFetcher({query, variables}) {
+  return {
+    subscribe(subscriber) {
+      const next = subscriber.next || subscriber;
+      const {error, complete} = subscriber;
+
+      const subscription = connection.subscribe({
+        query: query,
+        variables: {},
+        observer: ({state, valid, error, value}) => {
+          if (valid) {
+            next({data: value});
+          } else {
+            next({state, error});
+          }
+        }
+      });
+
+      return {
+        unsubscribe() {
+          return subscription.close();
+        }
+      };
+    }
+  };
+}
+
+ReactDOM.render(
+  <GraphiQL fetcher={graphQLFetcher} />,
+  document.getElementById('root')
+);
