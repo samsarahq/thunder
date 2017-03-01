9f38d7545cfe7b636af01c1e756d9098aee1a082
Jelle van den Hooff <jelle@samsara.com>
2017-03-01 14:23:24 -0800

    graphql: add schemabuilder support for NonNull and update introspection test
    Add schemabuilder support for NonNull in read queries (mutations are
    not yet supported.)
    
    The schemabuilder follows some simple rules to determine if a type is
    NonNull or not:
    - For scalar and struct types, a pointer type is nullable, and a
      non-pointer type is not.
    - For list fields, all lists and their items are non-nullable. The
      executor already enforced that lists are non-nullable, and in
    practice, our list items are never nullable.
    
    Change the introspection test to use its own schema. Manually inspected
    the schema and it looks reasonable.

--- graphql/introspection/introspection_test.go
+++ graphql/introspection/introspection_test.go
@@ -3,16 +3,49 @@ package introspection_test
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
 	"reflect"
 	"testing"
 
-	"github.com/samsarahq/thunder/example"
 	"github.com/samsarahq/thunder/graphql/introspection"
+	"github.com/samsarahq/thunder/graphql/schemabuilder"
 )
 
+type User struct {
+	Name     string
+	MaybeAge *int64
+}
+
+func makeSchema() *schemabuilder.Schema {
+	schema := schemabuilder.NewSchema()
+
+	query := schema.Query()
+	query.FieldFunc("me", func() User {
+		return User{Name: "me"}
+	})
+	query.FieldFunc("noone", func() *User {
+		return nil
+	})
+
+	user := schema.Object("User", User{})
+	user.FieldFunc("friends", func(u *User) []*User {
+		return nil
+	})
+	user.FieldFunc("greet", func(args struct {
+		Other   string
+		Include *User
+	}) string {
+		return ""
+	})
+
+	mutation := schema.Mutation()
+	mutation.FieldFunc("sayHi", func() {})
+
+	return schema
+}
+
 func TestComputeSchemaJSON(t *testing.T) {
-	server := example.Server{}
-	schemaBuilderSchema := server.SchemaBuilderSchema()
+	schemaBuilderSchema := makeSchema()
 
 	actualBytes, err := introspection.ComputeSchemaJSON(*schemaBuilderSchema)
 	if err != nil {
@@ -20,6 +53,10 @@ func TestComputeSchemaJSON(t *testing.T) {
 	}
 	var actual map[string]interface{}
 	json.Unmarshal(actualBytes, &actual)
+
+	if os.Getenv("UPDATE_TEST_RESULTS") != "" {
+		ioutil.WriteFile("test-schema.json", actualBytes, 0644)
+	}
 
 	expectedBytes, err := ioutil.ReadFile("test-schema.json")
 	if err != nil {
