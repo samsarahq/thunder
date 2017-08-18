01fb369390db85e227ac4bee126af5ada8f421d0
Stephen Wan <stephen@stephenwan.com>
2017-08-17 21:27:46 -0700

    schemabuilder: add support for Nullable variadic option (breaking change)
    Note that this is a breaking change to the API. After this commit, resolvers
    are assumed to be non-nullable by default and can be marked nullable
    using the Nullable option passed to the FieldFunc.

--- graphql/schemabuilder/types.go
+++ graphql/schemabuilder/types.go
@@ -11,6 +11,16 @@ type Object struct {
 	key string
 }
 
+// FieldFuncOption is an interface for the variadic options that can be passed
+// to a FieldFunc for configuring options on that function.
+type FieldFuncOption func(*method)
+
+// NonNullable is an option that can be passed to a FieldFunc to indicate that
+// its return value is required, even if the return value is a pointer type.
+func NonNullable(m *method) {
+	m.MarkedNonNullable = true
+}
+
 // FieldFunc exposes a field on an object. The function f can take a number of
 // optional arguments:
 // func([ctx context.Context], [o *Type], [args struct {}]) ([Result], [error])
@@ -29,16 +39,27 @@ type Object struct {
 //        userID, err := db.AddUser(ctx, args.FirstName, args.LastName)
 //        return userID, err
 //    })
-func (s *Object) FieldFunc(name string, f interface{}) {
+func (s *Object) FieldFunc(name string, f interface{}, options ...FieldFuncOption) {
 	if s.Methods == nil {
 		s.Methods = make(Methods)
 	}
-	s.Methods[name] = f
+
+	m := &method{Fn: f}
+	for _, option := range options {
+		option(m)
+	}
+
+	s.Methods[name] = m
 }
 
 func (s *Object) Key(f string) {
 	s.key = f
 }
 
+type method struct {
+	MarkedNonNullable bool
+	Fn                interface{}
+}
+
 // A Methods map represents the set of methods exposed on a Object.
-type Methods map[string]interface{}
+type Methods map[string]*method
