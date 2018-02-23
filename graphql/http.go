de5c47274ad5419f95d77a1607e5ea73e8c27509
Stephen Wan <stephen@stephenwan.com>
2018-02-22 22:53:46 -0800

    graphql: provide a non-live http interface

---
+++ graphql/http.go
@@ -0,0 +1,121 @@
+package graphql
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"sync"
+
+	"github.com/samsarahq/thunder/batch"
+	"github.com/samsarahq/thunder/reactive"
+)
+
+func HTTPHandler(schema *Schema, middlewares ...MiddlewareFunc) http.Handler {
+	return &httpHandler{
+		schema:      schema,
+		middlewares: middlewares,
+	}
+}
+
+type httpHandler struct {
+	schema      *Schema
+	middlewares []MiddlewareFunc
+}
+
+type httpPostBody struct {
+	Query     string                 `json:"query"`
+	Variables map[string]interface{} `json:"variables"`
+}
+
+type httpResponse struct {
+	Data   interface{} `json:"data"`
+	Errors []string    `json:"errors"`
+}
+
+func (h *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	writeResponse := func(value interface{}, err error) {
+		response := httpResponse{}
+		if err != nil {
+			response.Errors = []string{err.Error()}
+		} else {
+			response.Data = value
+		}
+
+		responseJSON, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Error(w, string(responseJSON), http.StatusOK)
+	}
+
+	if r.Method != "POST" {
+		writeResponse(nil, errors.New("request must be a POST"))
+		return
+	}
+
+	if r.Body == nil {
+		writeResponse(nil, errors.New("request must include a query"))
+		return
+	}
+
+	var params httpPostBody
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		writeResponse(nil, err)
+		return
+	}
+
+	query, err := Parse(params.Query, params.Variables)
+	if err != nil {
+		writeResponse(nil, err)
+		return
+	}
+
+	if err := PrepareQuery(h.schema.Query, query.SelectionSet); err != nil {
+		writeResponse(nil, err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	e := Executor{}
+
+	wg.Add(1)
+	runner := reactive.NewRerunner(r.Context(), func(ctx context.Context) (interface{}, error) {
+		defer wg.Done()
+
+		ctx = batch.WithBatching(ctx)
+
+		var middlewares []MiddlewareFunc
+		middlewares = append(middlewares, h.middlewares...)
+		middlewares = append(middlewares, func(input *ComputationInput, next MiddlewareNextFunc) *ComputationOutput {
+			output := next(input)
+			output.Current, output.Error = e.Execute(input.Ctx, h.schema.Query, nil, input.ParsedQuery)
+			return output
+		})
+
+		output := runMiddlewares(middlewares, &ComputationInput{
+			Ctx:         ctx,
+			ParsedQuery: query,
+			Query:       params.Query,
+			Variables:   params.Variables,
+		})
+		current, err := output.Current, output.Error
+
+		if err != nil {
+			if extractPathError(err) == context.Canceled {
+				return nil, err
+			}
+
+			writeResponse(nil, err)
+			return nil, err
+		}
+
+		writeResponse(current, nil)
+		return nil, nil
+	}, MinRerunInterval)
+
+	wg.Wait()
+	runner.Stop()
+}
