e1322efaf3a41514fa7ec55907c3fe6cb8748de5
Stephen Wan <stephen@stephenwan.com>
2018-05-28 11:44:11 -0700

    thunder-react: add ts library

---
+++ client/src/basicwebsocket.ts
@@ -0,0 +1,17 @@
+export interface BasicWebSocket {
+  readonly readyState: number;
+
+  onclose: (ev: CloseEvent) => any;
+  onerror: (ev: Event) => any;
+  onmessage: (ev: MessageEvent) => any;
+  onopen: (ev: Event) => any;
+  close(code?: number, reason?: string): void;
+  send(data: any): void;
+}
+
+export const BasicWebSocket = Object.freeze({
+  CONNECTING: 0,
+  OPEN: 1,
+  CLOSING: 2,
+  CLOSED: 3,
+});
