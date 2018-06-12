da604feb9da26207f12ece684152ae03b861930b
Stephen Wan <stephen@stephenwan.com>
2018-06-12 00:47:07 -0700

    thunder-react: get hoc to use spec
    This code is kind of horrifying, but it works.

--- client/src/hoc.tsx
+++ client/src/hoc.tsx
@@ -1,19 +1,57 @@
 import React from "react";
 import { GraphQLData } from "./connection";
-import { Query } from "./query";
+import { Query, QuerySpec } from "./query";
+import { Omit } from "./diff";
 
 export function graphql<
-  InnerProps,
+  InnerProps extends GraphQLData<Result>,
   Result extends object,
   Input extends object
->(query: string, variables: (props: InnerProps) => Input) {
-  return (
-    Component:
-      | React.ComponentClass<InnerProps & GraphQLData<Result>>
-      | React.StatelessComponent<InnerProps & GraphQLData<Result>>,
-  ): React.SFC<InnerProps> => {
-    return (props: InnerProps) => (
-      <Query<Result, Input> query={query} variables={variables(props)}>
+>(
+  query: string,
+  variables: (props: Omit<InnerProps, "data">) => Input,
+): <ChildProps extends InnerProps>(
+  Component: React.ComponentType<ChildProps>,
+) => React.SFC<Omit<ChildProps, "data">>;
+
+export function graphql<
+  InnerProps extends GraphQLData<Result>,
+  Result extends object,
+  Input extends object
+>(
+  query: QuerySpec<Result, Input>,
+  variables: (props: Omit<InnerProps, "data">) => Input,
+): <ChildProps extends InnerProps>(
+  Component: React.ComponentType<ChildProps>,
+) => React.SFC<Omit<ChildProps, "data">>;
+
+export function graphql<
+  InnerProps extends GraphQLData<Result>,
+  Result extends object,
+  Input extends object
+>(
+  query: string | QuerySpec<Result, Input>,
+  variables: (
+    props: Omit<InnerProps, "data">,
+  ) => typeof query extends string
+    ? Input
+    : Exclude<Exclude<typeof query, string>["variables"], undefined>,
+) {
+  type ResultType = typeof query extends string
+    ? Result
+    : Exclude<Exclude<typeof query, string>["result"], undefined>;
+  type InputType = typeof query extends string
+    ? Result
+    : Exclude<Exclude<typeof query, string>["variables"], undefined>;
+
+  return <ChildProps extends InnerProps>(
+    Component: React.ComponentType<ChildProps>,
+  ): React.SFC<Omit<ChildProps, "data">> => {
+    return props => (
+      <Query<ResultType, InputType>
+        query={query as string | QuerySpec<ResultType, InputType>}
+        variables={variables(props)}
+      >
         {data => <Component data={data} {...props} />}
       </Query>
     );
