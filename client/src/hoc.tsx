6345542074178b269d2b904b8e87e8bc805e7c33
Stephen Wan <stephen@stephenwan.com>
2018-06-20 01:10:37 +0200

    thunder-react: support hoc without variables

--- client/src/hoc.tsx
+++ client/src/hoc.tsx
@@ -5,6 +5,22 @@ import { Omit } from "./diff";
 
 export function graphql<
   InnerProps extends GraphQLData<Result>,
+  Result extends object
+>(
+  Component: React.ComponentType<InnerProps>,
+  query: string,
+): React.SFC<Omit<InnerProps, "data">>;
+
+export function graphql<
+  InnerProps extends GraphQLData<Result>,
+  Result extends object
+>(
+  Component: React.ComponentType<InnerProps>,
+  query: QuerySpec<Result>,
+): React.SFC<Omit<InnerProps, "data">>;
+
+export function graphql<
+  InnerProps extends GraphQLData<Result>,
   Result extends object,
   Input extends object
 >(
@@ -26,15 +42,17 @@ export function graphql<
 export function graphql<
   InnerProps extends GraphQLData<Result>,
   Result extends object,
-  Input extends object
+  Input extends object | undefined = undefined
 >(
   Component: React.ComponentType<InnerProps>,
   query: string | QuerySpec<Result, Input>,
-  variables: (
-    props: Omit<InnerProps, "data">,
-  ) => typeof query extends string
-    ? Input
-    : Exclude<Exclude<typeof query, string>["variables"], undefined>,
+  variables?:
+    | ((
+        props: Omit<InnerProps, "data">,
+      ) => typeof query extends string
+        ? Input
+        : Exclude<Exclude<typeof query, string>["variables"], undefined>)
+    | undefined,
 ): React.SFC<Omit<InnerProps, "data">> {
   type ResultType = typeof query extends string
     ? Result
@@ -46,7 +64,7 @@ export function graphql<
   return props => (
     <Query<ResultType, InputType>
       query={query as string | QuerySpec<ResultType, InputType>}
-      variables={variables(props)}
+      variables={variables ? variables(props) : {}}
     >
       {data => <Component data={data} {...props} />}
     </Query>
