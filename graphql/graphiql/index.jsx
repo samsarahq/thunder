ccb0f5969b4fd39655baf53aae3ac2f519e4052a
stevenyue <steven.yue@samsara.com>
2018-06-27 13:12:06 -0700

    graphiql/statik: support mutations from graphiql

--- graphql/graphiql/index.jsx
+++ graphql/graphiql/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GraphiQL from 'graphiql';
-import { connection } from 'thunder-react';
+import { connection, mutate } from 'thunder-react';
 
 import './node_modules/graphiql/graphiql.css';
 
 function graphQLFetcher({query, variables}) {
+  if (query.startsWith("mutation")) {
+    return mutate({
+      query,
+      variables,
+    });
+  }
   return {
     subscribe(subscriber) {
       const next = subscriber.next || subscriber;
