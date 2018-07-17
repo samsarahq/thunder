bd9d65b6a60a18f6f56ce7f1cd60d6ea3a93f26b
Shahryar Tariq <shahryar.tariq@samsara.com>
2018-07-17 12:53:53 -0700

    graphql schemabuilder: add slicing logic conforming to Relay Spec to return a Connection Type

--- graphql/schemabuilder/pagination.go
+++ graphql/schemabuilder/pagination.go
@@ -33,6 +33,16 @@ type Edge struct {
 	Cursor string
 }
 
+// ConnectionArgs conform to the pagination arguments as specified by the Relay Spec for Connection
+// types. The Args field consits of the user-facing args.
+type ConnectionArgs struct {
+	First  *int64
+	Last   *int64
+	After  *string
+	Before *string
+	Args   interface{}
+}
+
 // constructEdgeType wraps the typ (which is the type of the Node) in an Edge type conforming to the
 // Relay spec.
 func (sb *schemaBuilder) constructEdgeType(typ reflect.Type) (graphql.Type, error) {
@@ -130,4 +140,135 @@ func (sb *schemaBuilder) constructConnType(typ reflect.Type) (graphql.Type, erro
 	return retObject, nil
 }
 
+// EdgesToReturn returns the slice of edges by appyling the pagination arguments. It also returns
+// the hasNextPage and hasPrevPage values respectively. The behavior is expected to conform to the
+// Relay Cursor spec: https://facebook.github.io/relay/graphql/connections.htm#EdgesToReturn()
+func EdgesToReturn(allEdges []Edge, before *string, after *string, first *int64, last *int64) ([]Edge, bool, bool, error) {
+	edges, elemsAfter, elemsBefore := applyCursorsToAllEdges(allEdges, before, after)
+
+	prevPage := false
+	nextPage := false
+
+	if first != nil {
+		if *first < 0 {
+			return nil, nextPage, prevPage, graphql.NewClientError("first should be a non-negative integer")
+		}
+		if len(edges) > int(*first) {
+			edges = edges[:int(*first)]
+			nextPage = true
+		}
+	}
+	if before != nil {
+		if elemsAfter {
+			nextPage = true
+		}
+	}
+
+	if last != nil {
+		if *last < 0 {
+			return nil, nextPage, prevPage, graphql.NewClientError("last should be a non-negative integer")
+		}
+		if len(edges) > int(*last) {
+			edges = edges[len(edges)-int(*last):]
+			prevPage = true
+		}
+	}
+	if after != nil {
+		if elemsBefore {
+			prevPage = true
+		}
+	}
+
+	return edges, nextPage, prevPage, nil
+}
+
+// getCursorIndex returns the index corresponding to the cursor in the slice.
+func getCursorIndex(edges []Edge, cursor string) int {
+	for i, val := range edges {
+		if val.Cursor == cursor {
+			return i
+		}
+	}
+	return -1
+}
+
+// applyCursorsToAllEdges returns the slice of edges after applying the after and before arguments.
+// It also implements part of the hasNextPage and hasPrevPage algorithm by returning if there are
+// elements after or before the arguments.
+func applyCursorsToAllEdges(allEdges []Edge, before *string, after *string) ([]Edge, bool, bool) {
+	edges := allEdges
+
+	elemsAfter := false
+	elemsBefore := false
+
+	if after != nil {
+		i := getCursorIndex(edges, *after)
+		if i != -1 {
+			edges = edges[i+1:]
+			if i != 0 {
+				elemsBefore = true
+			}
+		}
+
+	}
+	if before != nil {
+		i := getCursorIndex(edges, *before)
+		if i != -1 {
+			edges = edges[:i]
+			if i != len(allEdges)-1 {
+				elemsAfter = true
+			}
+		}
+
+	}
+
+	return edges, elemsAfter, elemsBefore
+
+}
+
+// getConnection applies the ConnectionArgs to nodes and returns the result in a wrapped Connection
+// type.
+func getConnection(key string, nodes []interface{}, args ConnectionArgs) (Connection, error) {
+	var edges []Edge
+
+	lim := int64(0)
+	if args.First != nil {
+		lim = *args.First
+	} else if args.Last != nil {
+		lim = *args.Last
+	}
+
+	var pages []string
+	for i, val := range nodes {
+		// Get the value of the key field and then b64 encode it for the cursor.
+		keyValue := reflect.ValueOf(val)
+		if keyValue.Kind() == reflect.Ptr {
+			keyValue = keyValue.Elem()
+		}
+		keyString := []byte(fmt.Sprintf("%v", keyValue.FieldByName(key).Interface()))
+		cursorVal := base64.StdEncoding.EncodeToString(keyString)
+		if (int64(i) % lim) == 0 {
+			pages = append(pages, cursorVal)
+		}
+		edges = append(edges, Edge{Node: val, Cursor: cursorVal})
+	}
+
+	edges, nextPage, prevPage, err := EdgesToReturn(edges, args.Before, args.After, args.First, args.Last)
+	if err != nil {
+		return Connection{}, err
+	}
+
+	endCursor := ""
+	if len(edges) > 0 {
+		endCursor = edges[len(edges)-1].Cursor
+	}
+	startCursor := ""
+	if len(edges) > 0 {
+		startCursor = edges[0].Cursor
+	}
+
+	pageInfo := PageInfo{HasNextPage: nextPage, EndCursor: endCursor, StartCursor: startCursor, HasPrevPage: prevPage, Pages: pages}
+
+	return Connection{Count: int64(len(nodes)), Edges: edges, PageInfo: pageInfo}, nil
+}
 
