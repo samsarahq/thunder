ccebbe9007f06b40b7448abc0eeabb7c53a238b2
Bernardo Farah <bernardo.farah@samsara.com>
2018-08-16 14:34:16 -0700

    go fields: Scanner satisfies sql.Scanner interface
    SQL values can be serialized into fields by creating a Scanner from
    their corresponding field's descriptor.

--- fields/sql.go
+++ fields/sql.go
@@ -1,10 +1,13 @@
 package fields
 
 import (
+	"database/sql"
 	"database/sql/driver"
 	"encoding"
 	"encoding/json"
+	"fmt"
 	"reflect"
+	"time"
 )
 
 // Valuer fulfills the sql/driver.Valuer interface which deserializes our
@@ -105,3 +108,162 @@ func (f Valuer) Value() (driver.Value, error) {
 
 var _ driver.Valuer = Valuer{}
 var _ driver.Valuer = &Valuer{}
+
+// Scanner fulfills the sql.Scanner interface which deserializes SQL values
+// into the type dictated by our descriptor.
+type Scanner struct {
+	*Descriptor
+	value   reflect.Value
+	isValid bool
+}
+
+// Interface returns the value deserialized into the scanner.
+func (s Scanner) Interface() interface{} {
+	if s.value.IsValid() {
+		return s.value.Interface()
+	}
+	return nil
+}
+
+// Scan satisfies the sql.Scanner interface.
+// The src value will be one of the following:
+//    int64
+//    float64
+//    bool
+//    []byte
+//    string
+//    time.Time
+//    nil - for NULL values
+func (s *Scanner) Scan(src interface{}) error {
+	// Get a value of the pointer of our type. The Scanner and Unmarshalers should
+	// only be implemented as dereference methods, since they would do nothing otherwise. Therefore
+	// we can safely assume that we should check for these interfaces on the pointer value.
+	s.value = reflect.New(s.Type)
+	i := s.value.Interface()
+	// Our value however should continue referencing a non-pointer for easier assignment.
+	s.value = s.value.Elem()
+
+	// If our interface supports sql.Scanner we can immediately short-circuit as this is what the
+	// MySQL driver would do.
+	if scanner, ok := i.(sql.Scanner); ok {
+		// If we have a scanner it will handle its own validity.
+		s.isValid = true
+		return scanner.Scan(src)
+	}
+
+	// Keep track of whether our value was empty.
+	s.isValid = src != nil
+
+	// Null values are simply set to zero. Because we're not holding on to pointers, we need to
+	// represent this as a boolean. This comes _after_ the scanner step, just in case the scanner
+	// handles nil differently.
+	if !s.isValid {
+		return nil
+	}
+
+	// Handle coercion into native types []byte and time.Time (this method will return an error if
+	// we don't handle them). These are pointers here because we want to pass around a pointer
+	// for interfaces.
+	switch i.(type) {
+	case *[]byte:
+		if str, ok := src.(string); ok {
+			s.value.Set(reflect.ValueOf([]byte(str)))
+			return nil
+		}
+		if b, ok := src.([]byte); ok {
+			bCopy := make([]byte, len(b), len(b))
+			copy(bCopy, b)
+			s.value.Set(reflect.ValueOf(bCopy))
+			return nil
+		}
+	case *time.Time:
+		if _, ok := src.(time.Time); ok {
+			s.value.Set(reflect.ValueOf(src))
+			return nil
+		}
+	}
+
+	// Override deserialization behavior with tags (these take precedence over how a type would
+	// usually be deserialized).
+	// Example:
+	// struct {
+	//   Blob proto.Blob `sql:",binary"`       // ensures that Unmarshal or UnmarshalBinary is used.
+	//   IP IP `sql:",string"`                 // ensures that its UnmarshalText method
+	//	                                       // is used for deserialization.
+	//   JSON map[string]string `sql:",json"`  // ensures that json.Unmarshal is used on the value.
+	// }
+	switch {
+	case s.Tags.Contains("binary"):
+		b, ok := src.([]byte)
+		if !ok {
+			return fmt.Errorf("binary column must be of type []byte, got %T", src)
+		}
+		if iface, ok := i.(unmarshaler); ok {
+			return iface.Unmarshal(b)
+		}
+		if iface, ok := i.(encoding.BinaryUnmarshaler); ok {
+			return iface.UnmarshalBinary(b)
+		}
+	case s.Tags.Contains("string"):
+		if str, ok := src.(string); ok {
+			src = []byte(str)
+		}
+		b, isByte := src.([]byte)
+		if !isByte {
+			return fmt.Errorf("string/text column must be of type []byte or string, got %T", src)
+		}
+		if iface, ok := i.(encoding.TextUnmarshaler); isByte && ok {
+			return iface.UnmarshalText(b)
+		}
+	case s.Tags.Contains("json"):
+		if str, ok := src.(string); ok {
+			src = []byte(str)
+		}
+		b, isByte := src.([]byte)
+		if !isByte {
+			return fmt.Errorf("json column must be of type string or []byte, got %T", src)
+		}
+		// Implicitly will check for json.Unmarshaler.
+		return json.Unmarshal(b, i)
+	}
+
+	// If a MySQL value can be coerced into our type, we do so here.
+	// This not only converts base types into their sql counterparts (like int64 -> int32) but also
+	// handles custom types (like string -> `type customString string`).
+	switch s.Kind {
+	case reflect.Bool:
+		b := sql.NullBool{}
+		if err := b.Scan(src); err != nil {
+			return err
+		}
+		s.value.Set(reflect.ValueOf(b.Bool).Convert(s.Type))
+		return nil
+	case
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		i := sql.NullInt64{}
+		if err := i.Scan(src); err != nil {
+			return err
+		}
+		s.value.Set(reflect.ValueOf(i.Int64).Convert(s.Type))
+		return nil
+	case reflect.Float32, reflect.Float64:
+		float := sql.NullFloat64{}
+		if err := float.Scan(src); err != nil {
+			return err
+		}
+		s.value.Set(reflect.ValueOf(float.Float64).Convert(s.Type))
+		return nil
+	case reflect.String:
+		str := sql.NullString{}
+		if err := str.Scan(src); err != nil {
+			return err
+		}
+		s.value.Set(reflect.ValueOf(str.String).Convert(s.Type))
+		return nil
+	}
+
+	return fmt.Errorf("couldn't coerce type %T into %T", src, i)
+}
+
+var _ sql.Scanner = &Scanner{}
