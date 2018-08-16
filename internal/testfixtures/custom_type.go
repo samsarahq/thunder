221d1802a37b8ddbb69278b4937ffe695be2a6a3
Bernardo Farah <bernardo.farah@samsara.com>
2018-08-16 13:45:19 -0700

    go testfixtures: test non-native SQL type in sqlgen/livesql

--- internal/testfixtures/custom_type.go
+++ internal/testfixtures/custom_type.go
@@ -5,25 +5,28 @@ import (
 	"fmt"
 )
 
-type CustomType []byte
+type CustomType [16]byte
 
 func CustomTypeFromString(s string) CustomType {
-	return CustomType(s)
+	b := []byte(s)
+	c := CustomType{}
+	copy(c[:], b)
+	return c
 }
 
 func (u CustomType) Value() (driver.Value, error) {
-	return []byte(u), nil
+	return []byte(u[:]), nil
 }
 
 func (u *CustomType) Scan(value interface{}) error {
 	switch value := value.(type) {
 	case nil:
 		u = nil
-	case []byte:
-		*u = make(CustomType, len(value))
-		copy(*u, value)
 	case string:
-		*u = CustomType(value)
+		b := []byte(value)
+		copy(u[:], b)
+	case []byte:
+		copy(u[:], value)
 	default:
 		return fmt.Errorf("cannot convert %v (of type %T) to %T", value, value, u)
 	}
