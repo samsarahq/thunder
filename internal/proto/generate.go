78847c65ab32fe3658f00b7b6be237cda2f58e55
Changping Chen <changping.chen@samsara.com>
2018-09-12 15:05:12 -0700

    Revert "go internal: add proto example for testing"
    This reverts commit 9a87d53a417f5774ace3fb5cb43d05fa13114caa.

--- internal/proto/generate.go
+++
@@ -1,3 +0,0 @@
-package proto
-
-//go:generate sh -c "docker run -v `pwd`:/defs namely/protoc-all:1.11 -f *.proto -l gogo && mv gen/pb-gogo/github.com/samsarahq/thunder/internal/proto/* . && rm -rf gen"
