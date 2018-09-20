3ff10ebbb1cb4bad2fa8462dfd9a62f80cd04122
Changping Chen <changping.chen@samsara.com>
2018-09-20 15:00:59 -0700

    go livesql: marshal non driver.Value type filters

--- livesql/marshal.go
+++ livesql/marshal.go
@@ -2,10 +2,12 @@ package livesql
 
 import (
 	"database/sql/driver"
+	"errors"
 	"fmt"
 	"reflect"
 	"time"
 
+	"github.com/samsarahq/thunder/internal/fields"
 	"github.com/samsarahq/thunder/sqlgen"
 	"github.com/samsarahq/thunder/thunderpb"
 )
@@ -62,26 +64,87 @@ func fieldToValue(field *thunderpb.Field) (driver.Value, error) {
 	}
 }
 
-func filterToProto(table string, filter sqlgen.Filter) (*thunderpb.SQLFilter, error) {
+// filterToProto takes a sqlgen.Filter, runs Valuer on each filter value, and returns a thunderpb.SQLFilter.
+func filterToProto(schema *sqlgen.Schema, tableName string, filter sqlgen.Filter) (*thunderpb.SQLFilter, error) {
+	table, ok := schema.ByName[tableName]
+	if !ok {
+		return nil, fmt.Errorf("unknown table: %s", tableName)
+	}
+
+	if filter == nil {
+		return &thunderpb.SQLFilter{Table: tableName}, nil
+	}
+
 	fields := make(map[string]*thunderpb.Field, len(filter))
 	for col, val := range filter {
+		column, ok := table.ColumnsByName[col]
+		if !ok {
+			return nil, fmt.Errorf("unknown column %s", col)
+		}
+
+		val, err := column.Descriptor.Valuer(reflect.ValueOf(val)).Value()
+		if err != nil {
+			return nil, err
+		}
+
 		field, err := valueToField(val)
 		if err != nil {
 			return nil, err
 		}
 		fields[col] = field
 	}
-	return &thunderpb.SQLFilter{Table: table, Fields: fields}, nil
+	return &thunderpb.SQLFilter{Table: tableName, Fields: fields}, nil
 }
 
-func filterFromProto(proto *thunderpb.SQLFilter) (string, sqlgen.Filter, error) {
+// filterFromProto takes a thunderpb.SQLFilter, runs Scanner on each field value, and returns a sqlgen.Filter.
+func filterFromProto(schema *sqlgen.Schema, proto *thunderpb.SQLFilter) (string, sqlgen.Filter, error) {
+	table, ok := schema.ByName[proto.Table]
+	if !ok {
+		return "", nil, fmt.Errorf("unknown table: %s", proto.Table)
+	}
+
+	scanners := table.Scanners.Get().([]interface{})
+	defer table.Scanners.Put(scanners)
+
 	filter := make(sqlgen.Filter, len(proto.Fields))
 	for col, field := range proto.Fields {
 		val, err := fieldToValue(field)
 		if err != nil {
 			return "", nil, err
 		}
-		filter[col] = val
+
+		column, ok := table.ColumnsByName[col]
+		if !ok {
+			return "", nil, fmt.Errorf("unknown column %s", col)
+		}
+
+		if !column.Descriptor.Ptr && val == nil {
+			return "", nil, errors.New("cannot unmarshal nil into non-pointer type")
+		}
+
+		scanner := scanners[column.Order].(*fields.Scanner)
+
+		// target is always a pointer.
+		var target, ptrptr reflect.Value
+		if column.Descriptor.Ptr {
+			// We need to hold onto this pointer-pointer in order to make the value addressable.
+			ptrptr = reflect.New(reflect.PtrTo(column.Descriptor.Type))
+			target = ptrptr.Elem()
+		} else {
+			target = reflect.New(column.Descriptor.Type)
+		}
+		scanner.Target(target)
+
+		if err := scanner.Scan(val); err != nil {
+			return "", nil, err
+		}
+
+		if column.Descriptor.Ptr {
+			filter[col] = target.Interface()
+		} else {
+			// Dereference pointer if column type is not a pointer.
+			filter[col] = target.Elem().Interface()
+		}
 	}
 	return proto.Table, filter, nil
 }
