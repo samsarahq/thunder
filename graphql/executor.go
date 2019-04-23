59b60881580a0e2996f913588bdbbc5185803be9
Stephen Wan <stephen@stephenwan.com>
2019-04-22 21:10:33 -0700

    graphql: add Unwrap support

--- graphql/executor.go
+++ graphql/executor.go
@@ -45,6 +45,10 @@ func ErrorCause(err error) error {
 	return err
 }
 
+func (pe *pathError) Unwrap() error {
+	return pe.inner
+}
+
 func (pe *pathError) Error() string {
 	var buffer bytes.Buffer
 	for i := len(pe.path) - 1; i >= 0; i-- {
