3c6513d2f9bf0f66f5d27205ae7ae9f7adede513
Will Hughes <william.hughes@samsara.com>
2019-04-24 17:27:24 -0700

    go testgraphql: Add abililty to test against all Executor types
    Summary: Adds the capability to run all tests against both executors.
    The next commit changes all the tests in thunder to run against both
    executors.

--- internal/testgraphql/snapshotter.go
+++ internal/testgraphql/snapshotter.go
@@ -2,18 +2,20 @@ package testgraphql
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/samsarahq/go/snapshotter"
 	"github.com/samsarahq/thunder/graphql"
+	"github.com/samsarahq/thunder/internal"
 	"github.com/stretchr/testify/require"
 )
 
 type Snapshotter struct {
 	*snapshotter.Snapshotter
-	t        *testing.T
-	executor graphql.Executor
-	schema   *graphql.Schema
+	t      *testing.T
+	schema *graphql.Schema
 }
 
 func NewSnapshotter(t *testing.T, schema *graphql.Schema) *Snapshotter {
@@ -49,12 +51,35 @@ func (s *Snapshotter) SnapshotQuery(name, query string, opts ...Option) {
 		return
 	}
 	require.NoError(s.t, err)
-	output, err := s.executor.Execute(context.Background(), s.schema.Query, nil, q)
 
-	if err != nil && opt.recordError {
-		s.Snapshot(name, struct{ Error string }{err.Error()})
-	} else {
-		require.NoError(s.t, err)
-		s.Snapshot(name, output)
+	var lastOutput interface{}
+	var lastErr error
+	runOnce := false
+	for _, executorAndName := range GetExecutors() {
+		output, err := executorAndName.Executor.Execute(context.Background(), s.schema.Query, nil, q)
+
+		if err != nil && opt.recordError {
+			s.Snapshot(fmt.Sprintf("%s%s", executorAndName.Name, name), struct{ Error string }{err.Error()})
+		} else {
+			require.NoError(s.t, err)
+			s.Snapshot(fmt.Sprintf("%s%s", executorAndName.Name, name), output)
+		}
+		if !runOnce {
+			lastOutput = output
+			lastErr = err
+			runOnce = true
+			continue
+		}
+		if err != nil {
+			require.NotNil(s.t, lastErr)
+			require.Equal(s.t, lastErr.Error(), err.Error())
+			continue
+		}
+		require.Nil(s.t, lastErr)
+		require.True(
+			s.t,
+			reflect.DeepEqual(internal.AsJSON(lastOutput), internal.AsJSON(output)),
+			"Snapshots for %q do no match between different executors", name,
+		)
 	}
 }
