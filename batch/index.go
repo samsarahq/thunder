f610cf1abab69c369a0e5843df64f3454b696987
Will Hughes <william.hughes@samsara.com>
2019-06-18 17:34:11 -0700

    go batch: Make batch.Index a struct type
    Summary: It's too easy right now to accidentally make a batch.Index from
    a device Id or equivalent.  This commit changes the batch.Index to be a
    struct type which cannot be explicitly created, you must use a function
    to create it.  This should help reduce all the invalid usages of the
    batch.Index.

--- batch/index.go
+++ batch/index.go
@@ -1,5 +1,15 @@
 package batch
 
-// Index is an alias for the int type, it should be used in all batch field
-// funcs.
-type Index int
+// Index is a "key" mapper for batch requests.  Its key is not special and
+// should be treated the same as an index in a slice.
+type Index struct {
+	key int
+}
+
+// NewIndex creates an index.  This should only be used by
+// generated code or the creator of batches,  The "Key" has no purpose and should
+// not be used elsewhere.
+func NewIndex(key int) Index {
+	// TODO generate a hash to really mess with people trying to recreate batches?
+	return Index{key: key}
+}
