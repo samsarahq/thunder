c973226c8d17f747544dedc13c6aaa2833c4501c
Alan Flores-Lopez <alan.floreslopez@samsara.com>
2019-10-16 16:22:14 -0700

    sqlgen errors: add wrapped error type for dynamic limit errors

---
+++ sqlgen/errors.go
@@ -0,0 +1,27 @@
+package sqlgen
+
+import (
+	"fmt"
+)
+
+// ErrorWithQuery is an error wrapper that includes
+// the clause and arguments of a sqlgen query.
+type ErrorWithQuery struct {
+	err    error         // the wrapped error
+	clause string        // the string clause of the query
+	args   []interface{} // the args of the query
+
+}
+
+func (e *ErrorWithQuery) Error() string {
+	if e.err == nil {
+		return ""
+	}
+	return fmt.Sprintf("Error: %s; query clause: '%s'; query args: '%v'\n", e.err.Error(), e.clause, e.args)
+}
+
+// Unwrap returns the error without the clause
+// or arguments.
+func (e *ErrorWithQuery) Unwrap() error {
+	return e.err
+}
