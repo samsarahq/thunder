f42495c6d4335e9d9381db80d1a5ecc4f4973722
Jelle van den Hooff <jelle@samsara.com>
2019-10-30 13:04:12 +0000

    plan

--- federation/executor.go
+++ federation/executor.go
@@ -414,15 +414,16 @@ func convert(query *graphql.RawSelectionSet) []*Selection {
 }
 
 // todo
+// project. expose introspection query
+// federate onto introspectoin server (!?!?!)
+// serve graphiql
+//
 // project. harden APIs
 // test malformed inputs
 // test incompatible schemas
 // test forward/backward schema rollout
 // multiple root fields
 // validate incoming queries
-//
-// project. expose introspection query
-// federate onto introspectoin server (!?!?!)
 //
 // project. fragments
 //
