f3fc43eb21d46a4ed976cc97a995912b2116d569
Will Hughes <william.hughes@samsara.com>
2020-01-14 17:29:10 -0800

    go proto: Generate a proto type without Marshal/Unmarshal methods
    Summary: Adds a new "SimpleExampleEvent" protobuf type that is generated
    without Marshal/Unmarshal methods.  This type will be valuable for tests
    to ensure that we don't need Marshal/Unmarshal methods to use a protobuf
    type.

--- internal/proto/generate.go
+++ internal/proto/generate.go
@@ -1,3 +1,3 @@
 package proto
 
-//go:generate sh -c "docker run -v `pwd`:/defs namely/protoc-all:1.11 -f *.proto -l gogo && mv gen/pb-gogo/github.com/samsarahq/thunder/internal/proto/* . && rm -rf gen"
+//go:generate sh -c "docker run -v `pwd`:/defs namely/protoc-all:1.11 -l gogo -d . && mv gen/pb-gogo/github.com/samsarahq/thunder/internal/proto/* . && rm -rf gen"
