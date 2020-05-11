be71fe616773afd8bdb0701f52848a4c2d9e4f33
Aria Malkani <arshia.malkani@samsara.com>
2020-05-11 13:24:01 -0700

    federation/server: server to create a multiple graphql servers
    Interface to sent a query in a protobuf format, unmarshal the query, and execute it locally on the executor

---
+++ federation/server.go
@@ -0,0 +1,226 @@
+package federation
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/samsarahq/go/oops"
+	"github.com/samsarahq/thunder/graphql"
+	"github.com/samsarahq/thunder/graphql/introspection"
+	"github.com/samsarahq/thunder/reactive"
+	"github.com/samsarahq/thunder/thunderpb"
+)
+
+// DirectExecutorClient is used to execute directly on any of the graphql servers
+type DirectExecutorClient struct {
+	Client thunderpb.ExecutorServer
+}
+
+func (c *DirectExecutorClient) Execute(ctx context.Context, req *graphql.Query) ([]byte, error) {
+	// marshal query into a protobuf
+	marshaled, err := marshalQuery(req)
+	if err != nil {
+		return nil, oops.Wrapf(err, "marshaling query")
+	}
+	// Make a request to the executor client with the query
+	resp, err := c.Client.Execute(ctx, &thunderpb.ExecuteRequest{
+		Query: marshaled,
+	})
+	if err != nil {
+		return nil, oops.Wrapf(err, "executing query")
+	}
+	return resp.Result, nil
+}
+
+// Server must implement thunderpb.ExecutorServer.
+var _ thunderpb.ExecutorServer = &Server{}
+
+type Server struct {
+	schema        *graphql.Schema
+	localExecutor graphql.ExecutorRunner
+}
+
+func NewServer(schema *graphql.Schema) (*Server, error) {
+	introspection.AddIntrospectionToSchema(schema)
+	localExecutor := graphql.NewExecutor(graphql.NewImmediateGoroutineScheduler())
+	return &Server{
+		schema:        schema,
+		localExecutor: localExecutor,
+	}, nil
+}
+
+// Execute unmarshals the protobuf query and executes it on the server
+func (s *Server) Execute(ctx context.Context, req *thunderpb.ExecuteRequest) (*thunderpb.ExecuteResponse, error) {
+	query, err := unmarshalQuery(req.Query)
+	if err != nil {
+		return nil, oops.Wrapf(err, "unmarshaling query")
+	}
+
+	var schema graphql.Type
+	switch query.Kind {
+	case "query":
+		schema = s.schema.Query
+	case "mutation":
+		schema = s.schema.Mutation
+	default:
+		return nil, fmt.Errorf("unknown kind %s", query.Kind)
+	}
+
+	if err := graphql.PrepareQuery(ctx, schema, query.SelectionSet); err != nil {
+		return nil, err
+	}
+
+	// We're using `reactive.NewRerunner` to ensure that the reactive cache is set up correctly,
+	// but we won't actually wait for the query to rerun if invalidated.
+	done := make(chan struct{})
+	var queryResponse *thunderpb.ExecuteResponse
+	var queryError error
+	rerunner := reactive.NewRerunner(ctx, func(ctx context.Context) (ret interface{}, err error) {
+		defer func() {
+			queryResponse = ret.(*thunderpb.ExecuteResponse)
+			queryError = err
+			close(done)
+		}()
+
+		res, err := s.localExecutor.Execute(ctx, schema, nil, query)
+		if err != nil {
+			return nil, fmt.Errorf("executing query: %v", err)
+		}
+
+		bytes, err := json.Marshal(res)
+		if err != nil {
+			return nil, oops.Wrapf(err, "unmarshalling json query response")
+		}
+
+		return &thunderpb.ExecuteResponse{
+			Result: bytes,
+		}, nil
+	}, time.Hour, false)
+
+	<-done
+
+	rerunner.Stop()
+	return queryResponse, queryError
+}
+
+// marshalPbSelections gets a selection set and marshals it into the protobuf format
+func marshalPbSelections(selectionSet *graphql.SelectionSet) (*thunderpb.SelectionSet, error) {
+	if selectionSet == nil {
+		return nil, nil
+	}
+
+	selections := make([]*thunderpb.Selection, 0, len(selectionSet.Selections))
+	for _, selection := range selectionSet.Selections {
+		children, err := marshalPbSelections(selection.SelectionSet)
+		if err != nil {
+			return nil, oops.Wrapf(err, "marshaling selections")
+		}
+
+		var args []byte
+		if selection.Args != nil {
+			var err error
+			args, err = json.Marshal(selection.Args)
+			if err != nil {
+				return nil, oops.Wrapf(err, "marshaling args")
+			}
+		}
+
+		selections = append(selections, &thunderpb.Selection{
+			Name:         selection.Name,
+			Alias:        selection.Alias,
+			SelectionSet: children,
+			Arguments:    args,
+		})
+	}
+
+	fragments := make([]*thunderpb.Fragment, 0, len(selectionSet.Fragments))
+	for _, fragment := range selectionSet.Fragments {
+		selections, err := marshalPbSelections(fragment.SelectionSet)
+		if err != nil {
+			return nil, oops.Wrapf(err, "marshaling fragments")
+		}
+		fragments = append(fragments, &thunderpb.Fragment{
+			On:           fragment.On,
+			SelectionSet: selections,
+		})
+	}
+
+	return &thunderpb.SelectionSet{
+		Selections: selections,
+		Fragments:  fragments,
+	}, nil
+}
+
+// unmarshalPbSelectionSet gets a protobuf for a selection set and unmarshal it into graphql selection set object
+func unmarshalPbSelectionSet(selectionSet *thunderpb.SelectionSet) (*graphql.SelectionSet, error) {
+	if selectionSet == nil {
+		return nil, nil
+	}
+
+	selections := make([]*graphql.Selection, 0, len(selectionSet.Selections))
+	for _, selection := range selectionSet.Selections {
+		children, err := unmarshalPbSelectionSet(selection.SelectionSet)
+		if err != nil {
+			return nil, oops.Wrapf(err, "unmarshaling selections")
+		}
+
+		var args map[string]interface{}
+		if len(selection.Arguments) != 0 {
+			if err := json.Unmarshal(selection.Arguments, &args); err != nil {
+				return nil, oops.Wrapf(err, "unmarshaling selection arguments")
+			}
+		}
+
+		selections = append(selections, &graphql.Selection{
+			Name:         selection.Name,
+			Alias:        selection.Alias,
+			SelectionSet: children,
+			Args:         args,
+		})
+	}
+
+	fragments := make([]*graphql.Fragment, 0, len(selectionSet.Fragments))
+	for _, fragment := range selectionSet.Fragments {
+		selections, err := unmarshalPbSelectionSet(fragment.SelectionSet)
+		if err != nil {
+			return nil, oops.Wrapf(err, "unmarshaling fragments")
+		}
+		fragments = append(fragments, &graphql.Fragment{
+			On:           fragment.On,
+			SelectionSet: selections,
+		})
+	}
+
+	return &graphql.SelectionSet{
+		Selections: selections,
+		Fragments:  fragments,
+	}, nil
+}
+
+// marshalQuery marshals a graphql query type into a protobuf
+func marshalQuery(query *graphql.Query) (*thunderpb.Query, error) {
+	selectionSet, err := marshalPbSelections(query.SelectionSet)
+	if err != nil {
+		return nil, oops.Wrapf(err, "marshaling query")
+	}
+	return &thunderpb.Query{
+		Name:         query.Name,
+		Kind:         query.Kind,
+		SelectionSet: selectionSet,
+	}, nil
+}
+
+// unmarshalQuery unmarshals a protobuf query type into the graphql query type
+func unmarshalQuery(query *thunderpb.Query) (*graphql.Query, error) {
+	selectionSet, err := unmarshalPbSelectionSet(query.SelectionSet)
+	if err != nil {
+		return nil, oops.Wrapf(err, "unmarshaling query")
+	}
+	return &graphql.Query{
+		Name:         query.Name,
+		Kind:         query.Kind,
+		SelectionSet: selectionSet,
+	}, nil
+}
