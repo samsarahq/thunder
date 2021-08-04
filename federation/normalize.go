samsarahq/thunder

commit 7be9ad108fcd6097f2f0c6bde5fefd5d23910b9b

normalize: fix a bug in mergeSameAlias
In mergeSameAlias, we make a shadow copy of each selection and then append
to copy.selectionSet.selections later.
Since append in GO does not always create a new slice, there is chance we
are appending to the original selectionSet.selections slice. This could cause
the recursively flatten function to run into an infinite loop in some cases.

This commit make a new selectionSet for each copy so we will not append to the
original selections later in the function.

federation/normalize.go
@@ -150,8 +150,14 @@ func mergeSameAlias(selections []*graphql.Selection) ([]*graphql.Selection, erro
 		if last == nil || selection.Alias != last.Alias {
 			// Make a copy of the selection so we can modify it below
 			// or when we flatten recursively later.
-			copy := *selection
-			selection = &copy
+			cp := *selection
+			if selection.SelectionSet != nil {
+				// Make a new SelectionSet for the copy so we will not append to the original slice later.
+				cp.SelectionSet = &graphql.SelectionSet{}
+				cp.SelectionSet.Selections = append(cp.SelectionSet.Selections, selection.SelectionSet.Selections...)
+				cp.SelectionSet.Fragments = append(cp.SelectionSet.Fragments, selection.SelectionSet.Fragments...)
+			}
+			selection = &cp
 			newSelections = append(newSelections, selection)
 			last = selection
 			continue

commit 504739a940012c9878721b599401cfe8628b80b8

go federation/normalize: clean up messed code
In https://github.com/samsarahq/thunder/pull/487/commits, I merged commit
7be9ad108fcd6097f2f0c6bde5fefd5d23910b9b accidentally.

This is to drop the bad commit.

federation/normalize.go
@@ -155,12 +155,6 @@ func mergeSameAlias(selections []*graphql.Selection) ([]*graphql.Selection, erro
 			// Make a copy of the selection so we can modify it below
 			// or when we flatten recursively later.
 			cp := *selection
-			if selection.SelectionSet != nil {
-				// Make a new SelectionSet for the copy so we will not append to the original slice later.
-				cp.SelectionSet = &graphql.SelectionSet{}
-				cp.SelectionSet.Selections = append(cp.SelectionSet.Selections, selection.SelectionSet.Selections...)
-				cp.SelectionSet.Fragments = append(cp.SelectionSet.Fragments, selection.SelectionSet.Fragments...)
-			}
 			selection = &cp
 			newSelections = append(newSelections, selection)
 			last = selection