b45e14c060f39410a98ad191e2b4c7c06af69044
Weilian Chu <weilian.chu@samsara.com>
2023-01-10 16:03:18 -0800

    Revert "go federation: append to schema syncer interface"
    This reverts commit f42297157a85824fc84f035e0a3a210c40a57d56.

--- federation/schema_syncer.go
+++ federation/schema_syncer.go
@@ -11,7 +11,7 @@ import (
 // SchemaSyncer has a function that checks if the schema has changed,
 // and if so updates the planner in the federated executor
 type SchemaSyncer interface {
-	FetchPlannerAndIntrospectionQueryResult(ctx context.Context) (*Planner, []byte, error)
+	FetchPlanner(ctx context.Context) (*Planner, error)
 }
 type IntrospectionSchemaSyncer struct {
 	executors     map[string]ExecutorClient
@@ -27,17 +27,17 @@ func NewIntrospectionSchemaSyncer(ctx context.Context, executors map[string]Exec
 	return ss
 }
 
-func (s *IntrospectionSchemaSyncer) FetchPlannerAndIntrospectionQueryResult(ctx context.Context) (*Planner, []byte, error) {
+func (s *IntrospectionSchemaSyncer) FetchPlanner(ctx context.Context) (*Planner, error) {
 	schemas := make(map[string]*IntrospectionQueryResult)
 	for server, client := range s.executors {
 		resp, err := fetchSchema(ctx, client, s.queryMetadata)
 		if err != nil {
-			return nil, nil, oops.Wrapf(err, "fetching schema %s", server)
+			return nil, oops.Wrapf(err, "fetching schema %s", server)
 		}
 		schema := resp.Result
 		var iq IntrospectionQueryResult
 		if err := json.Unmarshal(schema, &iq); err != nil {
-			return nil, nil, oops.Wrapf(err, "unmarshaling schema %s", server)
+			return nil, oops.Wrapf(err, "unmarshaling schema %s", server)
 		}
 
 		schemas[server] = &iq
@@ -45,31 +45,25 @@ func (s *IntrospectionSchemaSyncer) FetchPlannerAndIntrospectionQueryResult(ctx 
 
 	types, err := convertSchema(schemas)
 	if err != nil {
-		return nil, nil, oops.Wrapf(err, "converting schemas error")
+		return nil, oops.Wrapf(err, "converting schemas error")
 	}
 
 	introspectionSchema := introspection.BareIntrospectionSchema(types.Schema)
 	schema, err := introspection.RunIntrospectionQuery(introspection.BareIntrospectionSchema(introspectionSchema))
 	if err != nil || schema == nil {
-		return nil, nil, oops.Wrapf(err, "error running introspection query")
+		return nil, oops.Wrapf(err, "error running introspection query")
 	}
 
 	var iq IntrospectionQueryResult
 	if err := json.Unmarshal(schema, &iq); err != nil {
-		return nil, nil, oops.Wrapf(err, "unmarshaling introspection schema")
+		return nil, oops.Wrapf(err, "unmarshaling introspection schema")
 	}
 
-	schemas[IntrospectionClientName] = &iq
-
+	schemas["introspection"] = &iq
 	types, err = convertSchema(schemas)
 	if err != nil {
-		return nil, nil, oops.Wrapf(err, "converting schemas error")
+		return nil, oops.Wrapf(err, "converting schemas error")
 	}
 
-	planner, err := NewPlanner(types, nil)
-	if err != nil {
-		return nil, nil, oops.Wrapf(err, "creating planner")
-	}
-
-	return planner, schema, nil
+	return NewPlanner(types, nil)
 }
